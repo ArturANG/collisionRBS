aef10d411cf8f0fe8f3fd19513c9f351821e624d
Artur Gomes <artur.gomes.ang@gmail.com>
2021-07-13 22:48:49 -0300

    Alteração da Densidade para 80

--- collision-lab/js/common/CollisionLabConstants.js
+++ collision-lab/js/common/CollisionLabConstants.js
@@ -47,7 +47,7 @@ const CollisionLabConstants = {
   MIN_VELOCITY: 1E-3,
 
   // Balls
-  BALL_DEFAULT_DENSITY: 35,   // Uniform Density of Balls if constant-radius is OFF, in kg/m^3.
+  BALL_DEFAULT_DENSITY: 80,   // Uniform Density of Balls if constant-radius is OFF, in kg/m^3.
   BALL_CONSTANT_RADIUS: 0.15, // Radius of Balls if constant-radius is on, in meters.
 
   // Momenta Diagram
